111519cd93178b1072557b61909e6eccb0d7e3b5
Leo Louis <leo.louis@yahoo.com>
2026-09-13 16:01:41 +0530

    Create App component for chit fund management

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Chit Fund Manager</h1>
+      <p>Welcome to your chit fund management application.</p>
+    </div>
+  );
+}
+
+export default App;
