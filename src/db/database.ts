09d6d989a5b13685f8e0156d19e0ce860cd4ee0d
Leo Louis <leo.louis@yahoo.com>
2026-09-13 16:47:56 +0530

    Refactor database imports and update schema

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,26 +1,21 @@
 import Dexie, { type Table } from "dexie";
 
-import type { Chit } from "../../types/chit";
-import type { Member } from "../../types/member";
-import type { Payment } from "../../types/payment";
-import type { Auction } from "../../types/auction";
-import type { Cycle } from "../../types/cycle";
+import type { Chit } from "../types/chit";
+import type { Member } from "../types/member";
+import type { Cycle } from "../types/cycle";
+import type { Payment } from "../types/payment";
+import type { Auction } from "../types/auction";
 
 export class ChitFundDatabase extends Dexie {
   chits!: Table<Chit, number>;
   members!: Table<Member, number>;
+  cycles!: Table<Cycle, number>;
   payments!: Table<Payment, number>;
   auctions!: Table<Auction, number>;
-  cycles!: Table<Cycle, number>;
 
   constructor() {
     super("ChitFundManager");
 
-    /*
-     * Database Version 1
-     *
-     * Original application database.
-     */
     this.version(1).stores({
       chits:
         "++id, name, status, startDate",
@@ -28,37 +23,16 @@ export class ChitFundDatabase extends Dexie {
       members:
         "++id, chitId, memberNumber, name, phone, status",
 
-      payments:
-        "++id, chitId, memberId, monthNumber, paymentDate",
-
-      auctions:
-        "++id, chitId, monthNumber, auctionDate, winnerMemberId",
-    });
-
-    /*
-     * Database Version 2
-     *
-     * Adds monthly chit cycles and
-     * connects payments to cycles.
-     */
-    this.version(2).stores({
-      chits:
-        "++id, name, status, startDate",
-
-      members:
-        "++id, chitId, memberNumber, name, phone, status",
-
-      payments:
-        "++id, chitId, memberId, cycleId, monthNumber, paymentDate",
-
-      auctions:
-        "++id, chitId, monthNumber, auctionDate, winnerMemberId",
-
       cycles:
         "++id, chitId, monthNumber, dueDate, status",
+
+      payments:
+        "++id, chitId, cycleId, memberId, monthNumber, paymentDate",
+
+      auctions:
+        "++id, chitId, monthNumber, auctionDate, winnerMemberId",
     });
   }
 }
 
-export const db =
-  new ChitFundDatabase();
+export const db = new ChitFundDatabase();
